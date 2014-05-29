3e2e359177e3f52aea730d1a41e45dc441daa104
norefle <norefle@outlook.com>
2014-05-30 00:43:44 +0200

    fl create: Creates cards file and its directory.

--- src/facil/init.lua
+++ src/facil/init.lua
@@ -3,20 +3,70 @@
 --- @brief Entry point of fácil module.
 ----------------------------------------------------------------------------]]--
 
-local lfs = require "lfs"
-local math = require "math"
-local io = require "io"
+local FileSystem = require "lfs"
+local Uuid = require "uuid"
 
 local _M = {}
 
+--- Generates full path inside .fl for card or meta files.
+-- @param root Root folder of file inside .fl ("cards", "meta") as string.
+-- @param prefix Name of subfolder inside the root as string.
+-- @return string with full path with trailing / on success, nil otherwise
+local function generatePath(root, prefix)
+    assert(root and "string" == type(root))
+    assert(prefix and "string" == type(prefix))
+
+    local pwd = FileSystem.currentdir()
+    if not pwd then
+        return nil
+    end
+
+    --local fullName = { pwd, "/.fl/", root, "/", prefix, "/", name }
+    return table.concat{ pwd, "/.fl/", root, "/", prefix, "/" }
+end
+
 --- Creates new card
--- @param cardName Short descriptive name of card.
+-- @param name Short descriptive name of card.
 -- @retval true, nil - on success.
 -- @retval nil, string - on error, where string contains detailed description.
-function _M.create(cardName)
-    if not cardName or "string" ~= type(cardName) then
-        return nil, "Invalid argument"
+function _M.create(name)
+    if not name or "string" ~= type(name) then
+        return nil, "Invalid argument."
     end
+
+    local card = {}
+
+    card.name = name
+    card.id = uuid.new()
+    if not card.id then
+        return nil, "Can't generate uuid for card."
+    end
+
+    card.time = os.time()
+
+    local prefix = card.id:sub(1, 2)
+    local body = card.id:sub(3)
+
+    local markdown = {}
+
+    markdown.path = generatePath("cards", prefix)
+    if not markdown.path then
+        return nil, "Can't generate file name for card."
+    end
+    markdown.name = markdown.path .. body .. ".md"
+
+    if not lfs.attributes(markdown.path)
+        and not lfs.mkdir(markdown.path)
+    then
+        return nil, "Can't create dir: " .. markdown.path
+    end
+
+    markdown.file = io.open(markdown.name, "w")
+    if not markdown.file then
+        return nil, "Can't create file: " .. tostring(markdown.name)
+    end
+
+    markdown.file:close()
 
     return true
 end
--- test/test_fl_create.lua
+++ test/test_fl_create.lua
@@ -18,17 +18,26 @@ end
 
 describe("fácil's create command", function()
     local fl -- Core module of fácil.
+    local uuid -- uuid library.
+    local lfs -- LuaFileSystem library.
 
-    setup(function()
+    before_each(function()
         -- Load and configure fácil module.
         fl = require "facil"
-        -- Set random generation in the same state every time.
-        math.randomseed(0)
+
+        -- Loads explicitly system libraries for mocking.
+        uuid = require "uuid"
+        lfs = require "lfs"
     end)
 
-    teardown(function()
-        -- Unload fácil module.
-        fl = nil
+    after_each(function()
+        -- Unload modules.
+        fl, package.loaded["facil"] = nil, nil
+        uuid, package.loaded["uuid"] = nil, nil
+        lfs, package.loaded["lfs"] = nil, nil
+
+        -- Removes all stubs and spies.
+        unwrap(io)
     end)
 
     it("exists", function()
@@ -38,16 +47,99 @@ describe("fácil's create command", function()
     it("fails with empty arguments", function()
         local result, details = fl.create()
         assert.is.equal(result, nil)
-        assert.is.equal(details, "Invalid argument")
+        assert.is.equal(details, "Invalid argument.")
     end)
 
     it("returns error on invalid file creation", function()
         io = mock(io, true)
+        lfs = mock(lfs, true)
+
+        local origin = {}
+        origin.currentdir = lfs.currentdir
+        origin.mkdir = lfs.mkdir
+
+        lfs.currentdir = function() return "/home/fl" end
+        lfs.mkdir = function() return true end
 
         local result, details = fl.create("name")
         assert.is.equal(result, nil)
-        assert.is.equal(details:find("Can't create temp file"), 1)
+        assert.is.equal(details:find("Can't create file: "), 1)
 
+        lfs.currentdir = origin.currentdir
+        lfs.mkdir = origin.mkdir
+
+        lfs = unwrap(lfs)
         io = unwrap(io)
+    end)
+
+    it("creates card and meta with valid names", function()
+        io = mock(io, true)
+        lfs = mock(lfs, true)
+
+        local origin = {}
+
+        origin.new = uuid.new
+        uuid.new = function() return "aaa-bbb-ccc-ddd" end
+
+        origin.currentdir = lfs.currentdir
+        lfs.currentdir = function() return "/xyz" end
+
+        origin.mkdir = lfs.mkdir
+        lfs.mkdir = function() return true end
+
+        local result, details = fl.create("new card")
+        -- Stub will return nil on io.open always,
+        -- as result fl.create should fail
+        assert.is.equal(result, nil)
+        assert.is.equal(details:find("Can't create file: "), 1)
+
+        -- Check that stub was called with correct arguments.
+        assert.stub(io.open).was.called(1)
+        assert.stub(io.open).was.called_with("/xyz/.fl/cards/aa/a-bbb-ccc-ddd.md", "w")
+
+        lfs.mkdir = origin.mkdir
+        lfs.currentdir = origin.currentdir
+        uuid.new = origin.new
+
+        lfs = unwrap(lfs)
+        io = unwrap(io)
+    end)
+
+    it("returns error if didn't get current directory", function()
+        lfs = mock(lfs, true)
+
+        local result, description = fl.create("wrong")
+        assert.is.equal(result, nil)
+        assert.is.equal("Can't generate file name for card.", description)
+
+        unwrap(lfs)
+    end)
+
+    it("creates required directories", function()
+        lfs = mock(lfs, true)
+
+        local origin = {
+            currentdir = lfs.currentdir,
+            mkdir = lfs.mkdir,
+            new = uuid.new
+        }
+
+        lfs.currentdir = function() return "/home/facil" end
+        lfs.mkdir = function(...)
+            origin.mkdir(...)
+            return true
+        end
+        uuid.new = function() return "1234-5678-9abc-ef01" end
+
+        local result, description = fl.create("task #1")
+
+        lfs.mkdir = origin.mkdir
+
+        assert.stub(lfs.mkdir).was.called(1)
+        assert.stub(lfs.mkdir).was.called_with("/home/facil/.fl/cards/12/")
+
+        uuid.new = origin.new
+        lfs.currentdir = origin.currentdir
+        unwrap(lfs)
     end)
 end)
